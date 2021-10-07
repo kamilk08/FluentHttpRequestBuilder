2c8e762972235e0f07d2b5df6d3c6bb27a1edd8e
BigKamil <allonder1992@gmail.com>
2021-10-07 12:45:12 +0200

    Add missing interface implementations

--- FluentHttpRequestBuilderLibrary/Constants/MediaType.cs
+++ FluentHttpRequestBuilderLibrary/Constants/MediaType.cs
@@ -1,9 +1,11 @@
-﻿namespace FluentHttpRequestBuilderLibrary.Constants
+﻿using System;
+
+namespace FluentHttpRequestBuilderLibrary.Constants
 {
-    public class MediaType
+    public class MediaType : IComparable,IEquatable<MediaType>
     {
         public int Value { get; }
-        
+
         public string Name { get; }
 
         public MediaType(int value, string name)
@@ -11,9 +13,59 @@
             Value = value;
             Name = name;
         }
+
+        internal static readonly MediaType ApplicationJson = new MediaType(1, "application/json");
+        internal static readonly MediaType OctetStream = new MediaType(2, "application/octet-stream");
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MediaType) obj);
+        }
+
+        public bool Equals(MediaType other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value == other.Value;
+        }
         
-        internal static readonly MediaType ApplicationJson = new MediaType(1,"application/json");
-        internal static readonly MediaType OctetStream = new MediaType(2,"application/octet-stream");
-        
+        public int CompareTo(object obj)
+        {
+            var mediaType = obj as MediaType;
+            if (mediaType == null) return 0;
+
+            return this.Value.CompareTo(mediaType.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Name.ToString();
+        }
+
+        public static bool operator ==(MediaType firstObject, MediaType secondObject)
+        {
+            if (ReferenceEquals(firstObject, null) && ReferenceEquals(secondObject, null))
+                return true;
+
+            if (ReferenceEquals(firstObject, null) || ReferenceEquals(secondObject, null))
+                return false;
+
+            return firstObject.Equals(secondObject);
+        }
+
+        public static bool operator !=(MediaType firstObject, MediaType secondObject)
+        {
+            return !(firstObject == secondObject);
+        }
+
+
     }
 }
--- FluentHttpRequestBuilderLibrary/Constants/ParameterType.cs
+++ FluentHttpRequestBuilderLibrary/Constants/ParameterType.cs
@@ -1,6 +1,8 @@
-﻿namespace FluentHttpRequestBuilderLibrary.Constants
+﻿using System;
+
+namespace FluentHttpRequestBuilderLibrary.Constants
 {
-    public class ParameterType
+    public class ParameterType : IEquatable<ParameterType>
     {
         public int Value { get; }
 
@@ -15,5 +17,48 @@
         public static readonly ParameterType ByteArray = new ParameterType(1, "ByteArray");
         public static readonly ParameterType Stream = new ParameterType(2, "Stream");
         public static readonly ParameterType String = new ParameterType(3, "String");
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (this.GetType() != obj.GetType()) return false;
+
+            return this.Equals((ParameterType) obj);
+        }
+
+        public bool Equals(ParameterType other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Name.ToString();
+        }
+
+        public static bool operator ==(ParameterType firstObject, ParameterType secondObject)
+        {
+            if (ReferenceEquals(firstObject, null) && ReferenceEquals(secondObject, null))
+                return true;
+
+            if (ReferenceEquals(firstObject, null) || ReferenceEquals(secondObject, null))
+                return false;
+
+            return firstObject.Equals(secondObject);
+        }
+
+        public static bool operator !=(ParameterType firstObject, ParameterType secondObject)
+        {
+            return !(firstObject == secondObject);
+        }
     }
 }
